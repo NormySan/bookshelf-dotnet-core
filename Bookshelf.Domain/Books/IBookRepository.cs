518355181a4a777993fe9a0ffc4b8f0e9f510976
Christoffer Palm <christoffer.palm@oddhill.se>
2021-10-23 21:59:01 +0200

    Book sorting and limit

--- Bookshelf.Domain/Books/IBookRepository.cs
+++ Bookshelf.Domain/Books/IBookRepository.cs
@@ -3,10 +3,35 @@ using System.Threading.Tasks;
 
 namespace Bookshelf.Domain.Books
 {
+    public enum SortByOptions
+    {
+        None,
+        Latest,
+        Rating,
+    }
+
+    public enum SortDirection
+    {
+        Ascending,
+        Descending,
+    }
+
+    public class SortingOptions
+    {
+        public SortByOptions SortBy { get; }
+        public SortDirection Direction { get; }
+
+        SortingOptions(SortByOptions sortBy, SortDirection direction)
+        {
+            SortBy = sortBy;
+            Direction = direction;
+        }
+    }
+
     public interface IBookRepository
     {
         public Task<Book> GetByIdAsync(int id);
 
-        public Task<List<Book>> GetAllAsync();
+        public Task<List<Book>> GetAllAsync(int limit, SortingOptions sorting);
     }
 }
